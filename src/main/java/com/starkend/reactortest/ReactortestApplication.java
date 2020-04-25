4fefeabefe437993f004c3be02fdc63696f3836e
starkend <danstarken@gmail.com>
2020-04-25 07:54:03 -0500

    Initial commit

---
+++ src/main/java/com/starkend/reactortest/ReactortestApplication.java
@@ -0,0 +1,13 @@
+package com.starkend.reactortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactortestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactortestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starkend/reactortest/ReactortestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starkend.reactortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactortestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
