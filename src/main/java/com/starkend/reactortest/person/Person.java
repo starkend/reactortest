6e94e969e7f97e5e7affc4ac8b460d7e06f5e629
starkend <danstarken@gmail.com>
2020-06-14 07:55:15 -0500

    Added Lombok AllArgsConstructor to Person entity class

--- src/main/java/com/starkend/reactortest/person/Person.java
+++ src/main/java/com/starkend/reactortest/person/Person.java
@@ -1,5 +1,6 @@
 package com.starkend.reactortest.person;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Log4j2
 @Data
 @Document
+@AllArgsConstructor
 public class Person {
 
     @Id
